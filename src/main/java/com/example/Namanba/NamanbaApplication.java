be33fbdfe509f4b740e657ad725362630f70bc48
CYJhub <yejee1018@naver.com>
2024-09-06 21:31:13 +0900

    chore: Initial setting (#1)

---
+++ src/main/java/com/example/Namanba/NamanbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Namanba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamanbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamanbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Namanba/NamanbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Namanba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamanbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
