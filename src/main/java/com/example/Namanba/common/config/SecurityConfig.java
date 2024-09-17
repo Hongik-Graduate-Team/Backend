72114ad9d34f0682ec443618541de6db86d9618a
혜원 <bogum1029@gmail.com>
2024-09-17 22:19:39 +0900

    feat: swaggerUrlPatterns 명시 URL 접근 허용 (#16)
    - securityConfig에 swaggerUrlPatterns 명시 url 허용
    - JwtAuthenticationFilter 파일 코드 변경 없이 코드 정렬

--- src/main/java/com/example/Namanba/common/config/SecurityConfig.java
+++ src/main/java/com/example/Namanba/common/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -28,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://deploy-preview-15--namanbatest.netlify.app","https://namanba.shop",
+        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://deploy-preview-15--namanbatest.netlify.app", "https://namanba.shop",
                 "https://main--namanbatest.netlify.app/"));
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
@@ -42,18 +43,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+        return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // jwt 사용하기 때문에 세션 비사용
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(swaggerUrlPatterns).permitAll()
                         .requestMatchers("/hello",
                                 "/api/auth/kakao-login",
                                 "/login/oauth2/code/kakao").permitAll()
                         .anyRequest().authenticated() // 나머지 경로는 인증 필요
                 )
-                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class);
-
-        return httpSecurity.build();
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
+
+    private final String[] swaggerUrlPatterns = {
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+    };
 
 }
