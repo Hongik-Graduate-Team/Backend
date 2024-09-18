6bd64fed5e4acd7ea8104250422b1649168f3cba
CYJhub <yejee1018@naver.com>
2024-09-18 22:34:45 +0900

    fix: JwtAuthenticationFilter 수정 (#19)

--- src/main/java/com/example/Namanba/common/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/Namanba/common/config/JwtAuthenticationFilter.java
@@ -75,86 +75,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return UserPrincipal.builder(user);
     }
 
-    /*
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws IOException, ServletException {
-
-        Cookie[] cookies = request.getCookies();
-
-        String authorization = null;
-
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("Authorization")) {
-                    authorization = cookie.getValue();
-                    break;
-                }
-            }
-        }
-
-        if (authorization == null || !authorization.startsWith("Bearer ")) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰 누락");
-            return;
-        }
-
-        String token = authorization.split(" ")[1];
-
-        try {
-            UserPrincipal userPrincipal = createPrincipalFromToken(token);
-
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                    userPrincipal, null, List.of(new SimpleGrantedAuthority("USER")));
-
-            authenticationToken.setDetails(
-                    new WebAuthenticationDetailsSource().buildDetails(request));
-
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        } catch (ExpiredJwtException e) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰 만료");
-            return;
-        } catch (Exception e) {
-            //logger.error(e.getMessage());
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "토큰 오류");
-            return;
-        }
-        filterChain.doFilter(request, response);
-    }
-
-    private UserPrincipal createPrincipalFromToken(String token) {
-        User user = userRepository.findById(jwtUtil.getUserId(token))
-                .orElseThrow(() -> new RuntimeException()); // 예외 처리 추후 수정
-
-        return UserPrincipal.builder(user);
-    }
-
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
-        List<String> allowedPath = Arrays.asList(
-                "/hello",
+        List<String> allowedPaths = Arrays.asList(
                 "/api/auth/kakao-login",
                 "/login/oauth2/code/kakao",
-                //"/api/portfolio",
-                //"/api/portfolio/position",
-                //"/api/career",
-                //"/api/certifications",
-                //"/api/majors",
-                //"/api/stacks",
-                //"/api/careers",
-                //"/api/language-certs",
-                //"/api/resumes",
-                //"/api/mypage/interview",
-                "/.well-known/acme-challenge/**",
-                "/v3/api-docs/**",
-                "/swagger-resources",
-                "/swagger-resources/**",
-                "/swagger-ui/**",
-                "/swagger-ui.html"
+                "/hello"
         );
 
         String path = request.getRequestURI();
+        return allowedPaths.stream().anyMatch(path::equals);
+    }
 
-        return allowedPath.stream().anyMatch(path::equals);
-    }*/
 
 }
