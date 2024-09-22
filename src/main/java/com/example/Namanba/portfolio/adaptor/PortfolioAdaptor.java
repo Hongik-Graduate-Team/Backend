585c3f0e6bb8f1d23b857c7dc46c032e1d746b61
혜원 <bogum1029@gmail.com>
2024-09-22 21:01:53 +0900

    fix: portfolio_id를 백엔드에서 자체적으로 찾는 기능으로 수정 (#14)

--- src/main/java/com/example/Namanba/portfolio/adaptor/PortfolioAdaptor.java
+++ src/main/java/com/example/Namanba/portfolio/adaptor/PortfolioAdaptor.java
@@ -3,9 +3,9 @@ package com.example.Namanba.portfolio.adaptor;
 import com.example.Namanba.common.annotation.Adaptor;
 import com.example.Namanba.common.exception.base.BaseException;
 import com.example.Namanba.portfolio.entity.Portfolio;
-import com.example.Namanba.portfolio.entity.subitems.Position;
 import com.example.Namanba.portfolio.exception.PortfolioErrorCode;
 import com.example.Namanba.portfolio.repository.PortfolioRepository;
+import com.example.Namanba.user.entity.User;
 import lombok.RequiredArgsConstructor;
 
 @Adaptor
@@ -15,6 +15,11 @@ public class PortfolioAdaptor {
 
     public Portfolio findByPortfolioId(Long portfolioId) {
         return portfolioRepository.findByPortfolioId(portfolioId)
+                .orElseThrow(() -> new BaseException(PortfolioErrorCode.PORTFOLIO_NOT_FOUND));
+    }
+
+    public Portfolio findByUser(User user) {
+        return portfolioRepository.findByUser(user)
                 .orElseThrow(() -> new BaseException(PortfolioErrorCode.PORTFOLIO_NOT_FOUND));
     }
 
