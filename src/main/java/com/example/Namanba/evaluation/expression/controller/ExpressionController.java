903b5313a1dac3d7fac06a7678f91ac4d4015328
CYJhub <yejee1018@naver.com>
2024-10-20 23:33:52 +0900

    feat: 면접 표정 평가 Service 생성 (#48)

--- src/main/java/com/example/Namanba/evaluation/expression/controller/ExpressionController.java
+++ src/main/java/com/example/Namanba/evaluation/expression/controller/ExpressionController.java
@@ -2,6 +2,7 @@ package com.example.Namanba.evaluation.expression.controller;
 
 import com.example.Namanba.common.response.SuccessResponse;
 import com.example.Namanba.evaluation.expression.dto.ExpressionDataDto;
+import com.example.Namanba.evaluation.expression.service.ExpressionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/{interviewId}/expression")
 public class ExpressionController {
+    private final ExpressionService expressionService;
 
     @PostMapping("")
     public SuccessResponse<Void> evaluateExpression(@RequestBody ExpressionDataDto expressionDataDto){
         // 면접 중 표정 평가 서비스 로직 구현
+        expressionService.evalExpression(expressionDataDto);
 
+        // 반환 값: 0~5 사이의 점수
         return new SuccessResponse<>(null);
     }
 }
---
+++ src/main/java/com/example/Namanba/evaluation/expression/service/ExpressionService.java
@@ -0,0 +1,14 @@
+package com.example.Namanba.evaluation.expression.service;
+
+import com.example.Namanba.evaluation.expression.dto.ExpressionDataDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExpressionService {
+
+    public void evalExpression(ExpressionDataDto expressionDataDto){
+
+    }
+}
