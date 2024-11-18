0da6bd846b4d499cac37d25f4d1d78cf7903292c
혜원 <bogum1029@gmail.com>
2024-11-18 16:36:37 +0900

    feat: 사용자 인터뷰 레포트 조회 기능 구현 (#71)
    - 인터뷰, 평가, 커스텀 질문 데이터를 포함한 응답 객체 생성
     - 평가 데이터는 평가가 완료된 경우에만 반환 (사용자가 인터뷰를 완료하지 않거나 평가 로직이 진행 중인 경우 제외)

---
+++ src/main/java/com/example/Namanba/Interview/usecase/ReadInterviewReportUseCase.java
@@ -0,0 +1,32 @@
+package com.example.Namanba.Interview.usecase;
+
+import com.example.Namanba.Interview.adaptor.CustomQuestionAdaptor;
+import com.example.Namanba.Interview.adaptor.InterviewAdaptor;
+import com.example.Namanba.Interview.dto.response.InterviewReportResponse;
+import com.example.Namanba.Interview.entity.CustomQuestion;
+import com.example.Namanba.Interview.entity.Interview;
+import com.example.Namanba.common.annotation.UseCase;
+import com.example.Namanba.common.util.JwtUtil;
+import com.example.Namanba.evaluation.adaptor.EvaluationAdaptor;
+import com.example.Namanba.evaluation.entity.Evaluation;
+import com.example.Namanba.user.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class ReadInterviewReportUseCase {
+    private final InterviewAdaptor interviewAdaptor;
+    private final EvaluationAdaptor evaluationAdaptor;
+    private final CustomQuestionAdaptor customQuestionAdaptor;
+    private final JwtUtil jwtUtil;
+
+    public InterviewReportResponse execute(Long interviewId,  HttpServletRequest httpRequest) {
+        User user = jwtUtil.getUserByToken(httpRequest);
+        Interview interview = interviewAdaptor.findByInterviewIdAndUser(interviewId, user);
+        Evaluation evaluation = evaluationAdaptor.findByOptionalInterview(interview)
+                .orElse(null);
+        CustomQuestion customQuestions = customQuestionAdaptor.findByInterview(interview);
+        return InterviewReportResponse.of(interview, evaluation, customQuestions);
+    }
+}
